5d5245ccf18588fd9058a5d2c283a278f03f36c7
soledadrv <soledadrv@outlook.com>
2021-11-22 11:29:06 -0300

    agrego validaciones

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,26 +1,27 @@
 const db = require('../database/models');
 const sequelize = db.sequelize;
+const { validationResult } = require('express-validator');
 
 //Otra forma de llamar a los modelos
 const Movies = db.Movie;
 
 const moviesController = {
     'list': (req, res) => {
-        db.Movie.findAll()
+        Movies.findAll()
             .then(movies => {
                 res.render('moviesList.ejs', {movies})
             })
     },
 
     'detail': (req, res) => {
-        db.Movie.findByPk(req.params.id)
+        Movies.findByPk(req.params.id)
             .then(movie => {
                 res.render('moviesDetail.ejs', {movie});
             });
     },
 
     'new': (req, res) => {
-        db.Movie.findAll({
+        Movies.findAll({
             order : [
                 ['release_date', 'DESC']
             ],
@@ -32,9 +33,9 @@ const moviesController = {
     },
 
     'recomended': (req, res) => {
-        db.Movie.findAll({
+        Movies.findAll({
             where: {
-                rating: {[db.Sequelize.Op.gte] : 8}
+                rating: {[sequelize.Op.gte] : 8}
             },
             order: [
                 ['rating', 'DESC']
@@ -51,20 +52,28 @@ const moviesController = {
 
     create: function (req, res) {
 
-        db.Movie.create(req.body)
+        const errors = validationResult(req);
 
-        .then(movie => {
-            res.redirect("/movies")
-        })
+        if (errors.isEmpty()) {
 
-        .catch(err => {
-            res.send(err)
-        })
+            Movies.create(req.body)
+
+            .then(movie => {
+                res.redirect("/movies")
+            })
+
+            .catch(err => {
+                res.send(err)
+            })
+
+        } else {
+            res.render('moviesAdd', {errors: errors.mapped(), old: req.body});
+        }
     },
 
     edit: function(req, res) {
 
-        db.Movie.findByPk(req.params.id)
+        Movies.findByPk(req.params.id)
 
         .then(Movie => {
             res.render('moviesEdit', {Movie})
@@ -76,22 +85,42 @@ const moviesController = {
     },
 
     update: function (req,res) {
+
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+
+            Movies.update(req.body,{
+                where: {id: req.params.id}
+            })
+    
+            .then(movie => {
+                res.redirect("/movies")
+            })
+    
+            .catch(err => {
+                res.send(err)
+            })
+            
+        } else {
+            
+            Movies.findByPk(req.params.id)
+
+            .then(Movie => {
+                res.render('moviesEdit', {Movie, errors: errors.mapped(), old: req.body})
+            })
+
+            .catch(err => {
+                res.send(err)
+            });
+        }
         
-        db.Movie.update(req.body,{
-            where: {id: req.params.id}
-        })
-
-        .then(movie => {
-            res.redirect("/movies")
-        })
-
-        .catch(err => {
-            res.send(err)
-        })
+        
     },
 
     delete: function (req, res) {
-        db.Movie.findByPk(req.params.id)
+
+        Movies.findByPk(req.params.id)
 
         .then(Movie => {
             res.render('moviesDelete', {Movie})
@@ -103,7 +132,8 @@ const moviesController = {
     },
 
     destroy: function (req, res) {
-        db.Movie.destroy({
+
+        Movies.destroy({
             where: {id: req.params.id}
         })
 
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/moviesController');
+const movieValidator = require('../validations/movieValidator');
 
 router.get('/movies', moviesController.list);
 router.get('/movies/new', moviesController.new);
@@ -10,10 +11,10 @@ router.get('/movies/detail/:id', moviesController.detail);
 
 //Rutas exigidas para la creación del CRUD
 router.get('/movies/add', moviesController.add);
-router.post('/movies/create', moviesController.create);
+router.post('/movies/create', movieValidator, moviesController.create);
 
 router.get('/movies/edit/:id', moviesController.edit);
-router.put('/movies/update/:id', moviesController.update);
+router.put('/movies/update/:id', movieValidator, moviesController.update);
 
 router.get('/movies/delete/:id', moviesController.delete);
 router.delete('/movies/delete/:id', moviesController.destroy);
